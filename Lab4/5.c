fa02d0bbaab807ea57418cf2d24c7d36930fbb6a
Inlucker <arseny707@mail.ru>
2021-11-18 06:23:34 +0300

    fixed task5

--- Lab4/5.c
+++ Lab4/5.c
@@ -3,21 +3,24 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 
-#define LEN 50
+#define LEN 60
 #define TEXT1 "My name is Proffesional\n"
 #define TEXT2 "There is no meaning in this words\n"
 
 void checkStatus(int child_pid, int status);
-void catch_sig(int sig_numb);
 
 int flag = 0;
 
+void catch_sig(int sig_numb)
+{
+	flag = 1;
+}
+
 int main()
 {
-    signal(SIGINT, catch_sig);
+	signal(SIGINT, catch_sig);
 	
 	int childpid_1, childpid_2;
 	int fd[2];
@@ -27,7 +30,7 @@ int main()
 		perror("Can\'t pipe.\n");
 		return EXIT_FAILURE;
 	}
-
+	
 	if ((childpid_1 = fork()) == -1)
 	{
 		perror("Can\'t fork.\n");
@@ -35,39 +38,39 @@ int main()
 	}
 	else if (childpid_1 == 0)
 	{
-		close(fd[0]);
-		write(fd[1], TEXT1, strlen(TEXT1) + 1);
+		sleep(2);
+		if (flag)
+		{
+			close(fd[0]);
+			write(fd[1], TEXT1, strlen(TEXT1));
+			printf("Child1 has sent the meassge\n");
+		}
+		else
+			printf("Child1 hasn't sent the message\n");
 		exit(EXIT_SUCCESS);
 	}
 
 	if ((childpid_2 = fork()) == -1)
 	{
 		perror("Can\'t fork.\n");
-		return EXIT_FAILURE;
+		return 1;
 	}
 	else if (childpid_2 == 0)
 	{
-		close(fd[0]);
-		write(fd[1], TEXT2, strlen(TEXT2) + 1);
-		exit(EXIT_SUCCESS);
+		sleep(2);
+		if (flag)
+		{
+			close(fd[0]);
+			write(fd[1], TEXT2, strlen(TEXT2));
+		}
+		else
+			printf("Child2 hasn't sent the message\n");
+			exit(EXIT_SUCCESS);
 	}
 
 	printf("Parent: pid = %d; pgrp = %d; child1 = %d; child2 = %d\n", getpid(), getpgrp(), childpid_1, childpid_2);
-	printf("Press \"CTRL+C\", to see message from second child.\n");
-	printf("In other case you will see message from first child.\n\n");
-	
-	char text1[LEN], text2[LEN];
-
-	close(fd[1]);
-	read(fd[0], text1, LEN);
-	read(fd[0], text2, LEN);
-	
-	sleep(2);
-	
-	if (flag)
-		printf("Message: %s", text2);
-	else
-		printf("Message: %s", text1);
+	printf("Press \"CTRL+C\", if you want childs to send messages.\n");
+	printf("In other case they will not.\n\n");
 
 	int status;
 	pid_t child_pid;
@@ -79,6 +82,20 @@ int main()
 	printf("Waiting...\n");
 	child_pid = wait(&status);
 	checkStatus(child_pid, status);
+	
+	if (flag)
+	{
+		char text[LEN] = "";
+		
+		close(fd[1]);
+		read(fd[0], text, LEN);
+		printf("Received message: %s\n", text);
+	}
+	
+	else
+	{
+		printf("No signal have been sent\n");
+	}
 
 	printf("Parent will die now.\n");
 	return EXIT_SUCCESS;
@@ -101,10 +118,4 @@ void checkStatus(int child_pid, int status)
 		printf("Child with pid = %d has stopped.\n", child_pid);
 		printf("Signal number = %d.", WSTOPSIG(status));
 	}
-}
-
-void catch_sig(int sig_numb)
-{
-	flag = 1;
-	printf("\ncatch_sig: %d\n", sig_numb);
 }
